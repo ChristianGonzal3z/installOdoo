6293cad25a0cf2bb0c4df230fc35f332ef9e1cde
Christian Z. Gonzalez <74432576+ChristianGonzal3z@users.noreply.github.com>
2023-11-08 15:20:01 -0400

    Update install_odoo.sh

--- install_odoo.sh
+++ install_odoo.sh
@@ -1,65 +1,70 @@
 #!/bin/bash
-echo "Que version de Odoo quiere instalar:"
-# shellcheck disable=SC2162
-read odoo_version
 
-echo "Actualizando el sistema"
-sudo apt update
-sudo apt upgrade -y
+# Función para verificar si la versión de Odoo es válida
+check_odoo_version() {
+  echo "Verificando si la versión de Odoo proporcionada existe..."
+  if ! git ls-remote --tags https://github.com/odoo/odoo.git "refs/tags/$odoo_version*" &> /dev/null; then
+    echo "La versión proporcionada de Odoo no existe o no está disponible como una etiqueta en el repositorio oficial."
+    echo "Por favor, verifique la versión deseada en el repositorio oficial: https://github.com/odoo/odoo/tags."
+    exit 1
+  fi
+}
 
-# Instalar dependencias
-sudo apt install git python3-pip build-essential wget python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less pwgen -y
+echo "¿Qué versión de Odoo desea instalar? (ejemplo: 13.0, 14.0, etc.):"
+read -r odoo_version
+check_odoo_version
 
-# Instalar PostgreSQL
+echo "Actualizando el sistema..."
+sudo apt update && sudo apt upgrade -y
+
+echo "Instalando dependencias necesarias para Odoo..."
+sudo apt install git python3-pip build-essential wget python3-dev python3-venv \
+python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools \
+node-less pwgen xz-utils -y
+
+echo "Instalando PostgreSQL..."
 sudo apt install postgresql -y
 sudo systemctl enable postgresql
 sudo systemctl start postgresql
 
-echo "Creando usuario de Odoo...."
+echo "Creando usuario de sistema para Odoo..."
 sudo useradd -m -U -r -d /opt/odoo -s /bin/bash odoo
 
-# Crear usuario de PostgreSQL para Odoo
+echo "Creando usuario de PostgreSQL para Odoo..."
 sudo su - postgres -c "createuser -s odoo"
 
-# Instalar WKHTMLTOPDF
+echo "Instalando WKHTMLTOPDF..."
+WKHTMLTOX_X64="https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb"
+wget -qO wkhtmltox.deb ${WKHTMLTOX_X64}
+sudo apt install -y ./wkhtmltox.deb
+rm -f wkhtmltox.deb
 
-echo "
---------------------------------------------------
- Instalando Wkhtmltopdf
---------------------------------------------------"
-WKHTMLTOX_X64=https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.bionic_amd64.deb
-wget ${WKHTMLTOX_X64}
-sudo apt install -y ./wkhtmltox_0.12.5-1.bionic_amd64.deb
-rm wkhtmltox_0.12.5-1.bionic_amd64.deb
+echo "Clonando el repositorio de Odoo..."
+sudo su - odoo -c "git clone --depth 1 --branch $odoo_version https://www.github.com/odoo/odoo /opt/odoo/odoo"
 
-# Clon el repositorio de Odoo
-sudo su - odoo -c "git clone https://www.github.com/odoo/odoo --depth 1 --branch $odoo_version /opt/odoo/odoo"
-
-# Crear entorno virtual de Python
+echo "Creando entorno virtual de Python para Odoo..."
 sudo su - odoo -c "python3 -m venv /opt/odoo/venv"
 
-# Instalar dependencias de Python
+echo "Instalando dependencias de Python para Odoo..."
+sudo su - odoo -c "/opt/odoo/venv/bin/pip install wheel"
 sudo su - odoo -c "/opt/odoo/venv/bin/pip install -r /opt/odoo/odoo/requirements.txt"
 
-# Generar contraseña aleatoria
+echo "Generando contraseña aleatoria para el usuario administrador de Odoo..."
 ODOO_ADMIN_PASSWD=$(pwgen 16 1)
 
-# Crear archivo de configuración de Odoo
-sudo touch /etc/odoo.conf
-sudo bash -c "cat > /etc/odoo.conf" << EOL
+echo "Creando archivo de configuración de Odoo..."
+sudo tee /etc/odoo.conf > /dev/null <<EOL
 [options]
-; This is the password that allows database operations:
 admin_passwd = ${ODOO_ADMIN_PASSWD}
 db_host = False
 db_port = False
 db_user = odoo
 db_password = False
-addons_path = /opt/odoo/odoo/addons,/opt/odoo/custom-addons
+addons_path = /opt/odoo/odoo/addons
 EOL
 
-# Crear archivo de servicio de Odoo
-sudo touch /etc/systemd/system/odoo.service
-sudo bash -c "cat > /etc/systemd/system/odoo.service" << EOL
+echo "Creando servicio de Odoo..."
+sudo tee /etc/systemd/system/odoo.service > /dev/null <<EOL
 [Unit]
 Description=Odoo
 Requires=postgresql.service
@@ -78,14 +83,15 @@ StandardOutput=journal+console
 WantedBy=multi-user.target
 EOL
 
-# Iniciar y habilitar el servicio de Odoo
+echo "Habilitando e iniciando el servicio de Odoo..."
 sudo systemctl daemon-reload
-sudo systemctl enable odoo.service
-sudo systemctl start odoo.service
+sudo systemctl enable --now odoo.service
 
-# Mostrar información de la instalación
-echo "La instalación de Odoo ${odoo_version} ha finalizado."
+echo "Instalación completada."
+echo "Datos de la instalación:"
 echo "Ubicación de la instalación: /opt/odoo"
-echo "Nombre de usuario: odoo"
-echo "Contraseña aleatoria del administrador: ${ODOO_ADMIN_PASSWD}"
-echo "Puedes acceder a la instancia en http://localhost:8069"
+echo "Usuario del sistema: odoo"
+echo "Contraseña del administrador de Odoo: ${ODOO_ADMIN_PASSWD}"
+echo "Acceso a Odoo: http://localhost:8069"
+
+# Nota: asegúrese de abrir el puerto 8069 si está utilizando un firewall.
